2c03af59fcf8041c371b95c320bf3bcd0fee79f5
alexross1787 <alexross1787@gmail.com>
2024-06-24 19:14:46 -0700

    fixed index route

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,14 +6,15 @@ uri = "mongodb://postgres.waffles@cluster-url/mydatabase?retryWrites=true&w=majo
 def create_app():
     app = Flask(__name__)
     
-        
-        # Register blueprints here
-        from . import profile
-        app.register_blueprint(profile.bp)
-
-        from . import feed
-        app.register_blueprint(feed.bp)
-        
-        return app
+    @app.route('/')
+    def index():
+        return "This is the index"
     
-   
+    # Register blueprints 
+    from . import profile
+    app.register_blueprint(profile.bp)
+
+    from . import feed
+    app.register_blueprint(feed.bp)
+        
+    return app
