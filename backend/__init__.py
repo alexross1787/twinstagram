bc89244980a74de13b0b57459c78d011d2484b1e
alexross1787 <alexross1787@gmail.com>
2024-06-24 18:20:13 -0700

    created mongodb db

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,21 +1,19 @@
-# App Factory 
-from flask import Flask
+from pymongo import MongoClient
+from flask import Flask, jsonify
 
+uri = "mongodb://postgres.waffles@cluster-url/mydatabase?retryWrites=true&w=majority&appName=Twinstagram"
 
 def create_app():
     app = Flask(__name__)
-
-    #index route
-    @app.route('/')
-    def index():
-        return "Twinstagram"
     
-    # register profile blueprint
-    from . import profile
-    app.register_blueprint(profile.bp)
+        
+        # Register blueprints here
+        from . import profile
+        app.register_blueprint(profile.bp)
 
-    from . import feed
-    app.register_blueprint(feed.bp)
-
+        from . import feed
+        app.register_blueprint(feed.bp)
+        
+        return app
     
-    return app
+   
--- backend/feed.py
+++ backend/feed.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
-import json 
-
-posts = json.load(open("profiles.json"))
+from flask import Blueprint, render_template, request, jsonify, g
 
 bp = Blueprint('feed', __name__, url_prefix='/feed')
 
 @bp.route('/')
 def index():
-    return "this is the feed index"
+    posts = list(g.mongo.post.find())
+    return render_template('feed/index.html', posts=posts)
+
+@bp.route('/create', methods=['POST'])
+def create_post():
+    data = request.json
+    g.mongo.post.insert_one(data)
+    return jsonify({'status': 'success', "message": "Post added"})
